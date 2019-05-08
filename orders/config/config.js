3a5f9faee406adc29bdb17db344596f6e3ed92dc
Vadym Motsukh <vadym.motsukh@gmail.com>
2019-05-08 09:22:02 +0300

    database config

---
+++ orders/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PSWD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PSWD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PSWD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    dialect: "postgres"
+  }
+}
