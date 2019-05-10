99acb9059462f9492078c9505f9f509ae45337c7
Vadym Motsukh <vadym.motsukh@gmail.com>
2019-05-10 22:32:01 +0300

    check if token empty

--- orders/helpers/authChecker.js
+++ orders/helpers/authChecker.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next){
   var token = req.headers['x-access-token'];
-  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  if (!token || token.length == 0) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
   //NOTE should be checking token process. Put always true for any token
   next();
